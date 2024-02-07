0ddd428b33ca80dc0e7e49554467845af23a7761
VP <ville-pekka.lahti@nemesys.fi>
2024-02-07 23:06:19 +0200

    fixes

--- initialProject/aiTalkAi.js
+++ initialProject/aiTalkAi.js
@@ -4,9 +4,13 @@ const ollama = new Ollama({ host: 'localhost:11434' });
 const ollama2 = new Ollama({ host: 'localhost:11435' });
 
 const formatInstruction = `Output JSON in this format: {"sentence":"Some example sentence."}. Nothing else should be returned.`;
-const initialInstruction = `Let's generate a tale sentence by sentence, only one at a time. ${formatInstruction}`;
+const initialInstruction = `Let's generate a tale about ${
+  process.env.TOPIC || '"incompleted environment variables in your Diploi Deployment"'
+}, sentence by sentence, only one at a time. ${formatInstruction}`;
 let taleSentences = [];
-const completionInstruction = `You are about to read a tale that is incompleted. Generate one more sentence to it. ${formatInstruction}. The tale begins here:`;
+const completionInstruction = `You are about to read a tale about ${
+  process.env.TOPIC || '"incompleted environment variables in your Diploi Deployment"'
+} that is incompleted. Generate one more sentence to it. ${formatInstruction}. The tale begins here:`;
 console.log('Initial prompt:', initialInstruction);
 while (true) {
   const resp = await ollama.generate({
