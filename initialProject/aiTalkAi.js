e4cf21f6dee9dd245f252d1aba15b242a55fc0f2
VP <ville-pekka.lahti@nemesys.fi>
2024-02-12 14:20:31 +0200

    fixes

--- initialProject/aiTalkAi.js
+++ initialProject/aiTalkAi.js
@@ -6,31 +6,42 @@ const ollama2 = new Ollama({ host: 'localhost:11435' });
 const formatInstruction = `Output JSON in this format: {"sentence":"Some example sentence."}. Nothing else should be returned.`;
 const initialInstruction = `Let's generate a tale about ${
   process.env.TOPIC || '"incompleted environment variables in your Diploi Deployment"'
-}, sentence by sentence, only one at a time. ${formatInstruction}`;
+}, sentence by sentence, only one at a time, no multiple sentences but only, ONLY one! ${formatInstruction}`;
 let taleSentences = [];
 const completionInstruction = `You are about to read a tale about ${
   process.env.TOPIC || '"incompleted environment variables in your Diploi Deployment"'
-} that is incompleted. Generate one more sentence to it. ${formatInstruction}. The tale begins here:`;
+} that is incompleted. Generate one sentence to it - only one at a time, no multiple. ${formatInstruction}. You are given the full story before your sentence.`;
 console.log('Initial prompt:', initialInstruction);
 while (true) {
   const resp = await ollama.generate({
     model: 'llama2',
     format: 'json',
     stream: false,
-    prompt: taleSentences.length ? `${completionInstruction}\n${taleSentences.join('\n')}` : initialInstruction,
+    system: completionInstruction,
+    options: {
+      repeat_penalty: 1.23,
+      penalize_newline: true,
+      temperature: 0.75,
+    },
+    prompt: taleSentences.length ? `${taleSentences.join('. ')}` : initialInstruction,
   });
   const { sentence } = JSON.parse(resp.response);
-  console.log('ollama1>', sentence);
+  console.log('llama>', sentence);
   taleSentences.push(sentence);
 
   const resp2 = await ollama2.generate({
     model: 'llama2',
     format: 'json',
     stream: false,
-    prompt: `${completionInstruction}\n${taleSentences.join('\n')}`,
+    system: completionInstruction,
+    options: {
+      repeat_penalty: 1.33,
+      penalize_newline: true,
+      temperature: 0.99,
+    },
+    prompt: `${taleSentences.join('. ')}`,
   });
-
   const { sentence: sentence2 } = JSON.parse(resp2.response);
-  console.log('ollama2>', sentence2);
+  console.log('another llama>', sentence2);
   taleSentences.push(sentence2);
 }
